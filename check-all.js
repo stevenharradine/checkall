05d2e9ca104e52f26a38f2d89b3659a624d7a1c9
Steven Harradine <stevenharradine@gmail.com>
2016-05-05 15:22:59 -0400

    fix tag case issue, refactor

--- check-all.js
+++ check-all.js
@@ -1,19 +1,25 @@
 var envs = require ("./" + process.argv[2]).Reservations
 
 envs.forEach (function (item) {
-  var tagFound = false
+  var tagProjectFound = false,
+      tagRoleFound = false,
+      project,
+      role
 
   process.stdout.write ("echo -n ")
   item.Instances[0].Tags.forEach (function (tag) {
     if (tag.Key === "Project") {
-      tagFound = true
-      process.stdout.write (tag.Value)
+      tagProjectFound = true
+      project = tag.Value
+    } else if (tag.Key === "Role") {
+      tagRoleFound = true
+      role = tag.Value
     }
   })
-  if (!tagFound) {
-    process.stdout.write (item.Instances[0].PublicDnsName)
+  if (!tagProjectFound) {
+    project = item.Instances[0].PublicDnsName
   }
-  console.log ("' '")
 
+  console.log (project + "' '")
   console.log ("ssh -o StrictHostKeyChecking=no " + item.Instances[0].PublicDnsName + " 'dpkg --get-selections | grep -v deinstall | grep imagemagic | wc -l'");
 })
